6cd3a7295ee0468bba52edcad356b8d9fbc14163
Maarten <Maarten@Maarten.home>
2021-03-09 21:47:47 +0100

    Added de_counter.sh

---
+++ de_counter.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# The wget will get the contents from the url below, it will then get all 'de' occurences. The \b is there to
+# make sure it is not occuring inside of another word. 
+wget https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen -q -O - | grep -o -i '\bde' | wc -l
