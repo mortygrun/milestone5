b30b417eb367d09be92026d1172e942c7a479353
mortygrun <78690321+mortygrun@users.noreply.github.com>
2021-03-10 12:13:38 +0100

    Update de_counter.sh

--- de_counter.sh
+++ de_counter.sh
@@ -2,4 +2,4 @@
 
 # The wget will get the contents from the url below, it will then get all 'de' occurences. The \b is there to
 # make sure it is not occuring inside of another word. 
-wget https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen -q -O - | grep -o -i '\bde' | wc -l
+wget https://nl.wikipedia.org/wiki/Rijksuniversiteit_Groningen -q -O - | grep -w -o -i '\bde' | wc -l
